75123c815e5135b48aa2ad697094fddf73052757
eva khofifah mulqia <evakhofifah999@gmail.com>
2023-05-22 21:57:58 +0700

    Upload Pertemuan 6

---
+++ Pert6_dom.js
@@ -0,0 +1,13 @@
+document.write('jelloworld');
+document.write('<h2>saya sedang belajar dom</h2>');
+
+document.head;
+
+console.log(document.title);
+
+//mengubah judul
+document.title = "Judul halaman baru";
+
+console.log(document.head);
+
+
